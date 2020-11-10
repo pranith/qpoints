1cc287313beda444f34802b75dc07e3cd5cd5475
Pranith Kumar <bobby.prani@gmail.com>
2020-11-10 13:40:53 -0800

    Add qemu coding style config file for emacs

---
+++ .dir-locals.el
@@ -0,0 +1,11 @@
+((nil . ((c-default-style "linux")
+         (indent-tabs-mode . nil)
+         (c-tab-always-indent . t)
+         (c-basic-offset . 4)
+         (c-hanging-braces-alist . (
+                    (brace-list-open)
+                    (brace-list-close)
+                    (block-close . c-snug-do-while)
+                    (substatement-open before after)))
+         (fill-column . 80)
+         (subdirs . nil))))
