fcbf078450c2ae7df9a4ca8813912a126e477819
Pranith Kumar <bobby.prani@gmail.com>
2020-11-12 14:13:22 -0800

    Updates

--- Makefile
+++ Makefile
@@ -1,8 +1,8 @@
 QEMU_DIR ?=
 GLIB_INC ?=`pkg-config --cflags glib-2.0`
-CXXFLAGS ?= -g -Wall -std=c++11 -march=native -I$(QEMU_DIR)/include/qemu/ $(GLIB_INC)
+CXXFLAGS ?= -g -Wall -std=c++11 -march=native -iquote $(QEMU_DIR)/include/qemu/ $(GLIB_INC)
 
 all: libbbv.so
 
 libbbv.so: bbv.cc
-	$(CXX) $(CXXFLAGS) -shared -fPIC -o $@ $<
+	$(CXX) $(CXXFLAGS) -shared -fPIC -o $@ $< -ldl -lrt
--- bbv.cc
+++ bbv.cc
@@ -2,135 +2,144 @@
  * Copyright (C) 2020, Pranith Kumar <bobby.prani@gmail.com>
  *
  */
+#include "qemu-plugin.h"
+
+#include <iostream>
+#include <sstream>
 
 #include <inttypes.h>
 #include <assert.h>
 #include <stdlib.h>
+#include <inttypes.h>
 #include <string.h>
 #include <unistd.h>
 #include <stdio.h>
 #include <glib.h>
-
-#include <qemu-plugin.h>
+#include <zlib.h>
 
 QEMU_PLUGIN_EXPORT int qemu_plugin_version = QEMU_PLUGIN_VERSION;
 
+/* Plugins need to take care of their own locking */
+static GMutex lock;
+static GHashTable *hotblocks;
+
+static uint64_t unique_trans_id = 0; /* unique id assigned to TB */
+static uint64_t inst_count = 0; /* executed instruction count */
+
+static gzFile bbv_file;
+
+/*
+ * Counting Structure
+ *
+ * The internals of the TCG are not exposed to plugins so we can only
+ * get the starting PC for each block. We cheat this slightly by
+ * xor'ing the number of instructions to the hash to help
+ * differentiate.
+ */
 typedef struct {
-    GMutex lock;
-    int index;
-    uint64_t bb_count;
-    uint64_t insn_count;
-} CPUCount;
+    uint64_t start_addr;
+    uint64_t exec_count;
+    uint64_t id;
+    int      trans_count;
+    unsigned long insns;
+} ExecCount;
 
-/* Used by the inline & linux-user counts */
-static bool do_inline;
-static CPUCount inline_count;
-
-/* Dump running CPU total on idle? */
-static bool idle_report;
-static GPtrArray *counts;
-static int max_cpus;
-
-static void gen_one_cpu_report(CPUCount *count, GString *report)
+static gint cmp_exec_count(gconstpointer a, gconstpointer b)
 {
-    if (count->bb_count) {
-        g_string_append_printf(report, "CPU%d: "
-                               "bb's: %" PRIu64", insns: %" PRIu64 "\n",
-                               count->index,
-                               count->bb_count, count->insn_count);
-    }
+    ExecCount *ea = (ExecCount *) a;
+    ExecCount *eb = (ExecCount *) b;
+    return (ea->exec_count * ea->insns) > (eb->exec_count * eb->insns) ? -1 : 1;
 }
 
 static void plugin_exit(qemu_plugin_id_t id, void *p)
 {
-    g_autoptr(GString) report = g_string_new("");
+    GList *it;
 
-    if (do_inline || !max_cpus) {
-        g_string_printf(report, "bb's: %" PRIu64", insns: %" PRIu64 "\n",
-                        inline_count.bb_count, inline_count.insn_count);
-    } else {
-        g_ptr_array_foreach(counts, (GFunc) gen_one_cpu_report, report);
+    g_mutex_lock(&lock);
+    it = g_hash_table_get_values(hotblocks);
+
+    if (it) {
+        g_list_free(it);
     }
-    qemu_plugin_outs(report->str);
+
+    g_mutex_unlock(&lock);
+    gzclose(bbv_file);
 }
 
-static void vcpu_idle(qemu_plugin_id_t id, unsigned int cpu_index)
+static void plugin_init(void)
 {
-    CPUCount *count = (CPUCount *)g_ptr_array_index(counts, cpu_index);
-    g_autoptr(GString) report = g_string_new("");
-    gen_one_cpu_report(count, report);
-
-    if (report->len > 0) {
-        g_string_prepend(report, "Idling ");
-        qemu_plugin_outs(report->str);
-    }
+    hotblocks = g_hash_table_new(NULL, g_direct_equal);
+    bbv_file = gzopen("bbv.gz", "w");
 }
 
-static void vcpu_tb_exec(unsigned int cpu_index, void *udata)
+static void tb_exec(unsigned int cpu_index, void *udata)
 {
-    CPUCount *count = max_cpus ?
-        (CPUCount *)g_ptr_array_index(counts, cpu_index) : &inline_count;
+    g_mutex_lock(&lock);
+    if (inst_count >= 100000000 /*interval*/) {
+        std::ostringstream bb_stat;
+        GList *counts, *it;
+        int i = 0;
 
-    uintptr_t n_insns = (uintptr_t)udata;
-    g_mutex_lock(&count->lock);
-    count->insn_count += n_insns;
-    count->bb_count++;
-    g_mutex_unlock(&count->lock);
-}
+        counts = g_hash_table_get_values(hotblocks);
+        it = g_list_sort(counts, cmp_exec_count);
+        bb_stat << "T";
 
-static void vcpu_tb_trans(qemu_plugin_id_t id, struct qemu_plugin_tb *tb)
-{
-    size_t n_insns = qemu_plugin_tb_n_insns(tb);
-
-    if (do_inline) {
-        qemu_plugin_register_vcpu_tb_exec_inline(tb, QEMU_PLUGIN_INLINE_ADD_U64,
-                                                 &inline_count.bb_count, 1);
-        qemu_plugin_register_vcpu_tb_exec_inline(tb, QEMU_PLUGIN_INLINE_ADD_U64,
-                                                 &inline_count.insn_count,
-                                                 n_insns);
-    } else {
-        qemu_plugin_register_vcpu_tb_exec_cb(tb, vcpu_tb_exec,
-                                             QEMU_PLUGIN_CB_NO_REGS,
-                                             (void *)n_insns);
-    }
-}
-
-QEMU_PLUGIN_EXPORT int qemu_plugin_install(qemu_plugin_id_t id,
-                                           const qemu_info_t *info,
-                                           int argc, char **argv)
-{
-    int i;
-
-    for (i = 0; i < argc; i++) {
-        char *opt = argv[i];
-        if (g_strcmp0(opt, "inline") == 0) {
-            do_inline = true;
-        } else if (g_strcmp0(opt, "idle") == 0) {
-            idle_report = true;
-        } else {
-            fprintf(stderr, "option parsing failed: %s\n", opt);
-            return -1;
+        while (it && i < 100) {
+            ExecCount *rec = (ExecCount *) it->data;
+            if (rec->exec_count) {
+                bb_stat << " :" << rec->id << ":" << rec->exec_count * rec->insns;
+                rec->exec_count = 0;
+            }
+            it = it->next;
+            i++;
         }
+
+        bb_stat << std::endl;
+        gzwrite(bbv_file, bb_stat.str().c_str(), bb_stat.str().length());
+        inst_count = 0;
+    }
+    g_mutex_unlock(&lock);
+}
+
+static void tb_record(qemu_plugin_id_t id, struct qemu_plugin_tb *tb)
+{
+    ExecCount *cnt;
+    uint64_t pc = qemu_plugin_tb_vaddr(tb);
+    size_t insns = qemu_plugin_tb_n_insns(tb);
+    uint64_t hash = pc ^ insns;
+
+    g_mutex_lock(&lock);
+    cnt = (ExecCount *) g_hash_table_lookup(hotblocks, (gconstpointer) hash);
+    if (cnt) {
+        cnt->trans_count++;
+    } else {
+        cnt = g_new0(ExecCount, 1);
+        cnt->start_addr = pc;
+        cnt->trans_count = 1;
+        cnt->id = ++unique_trans_id;
+        cnt->insns = insns;
+        g_hash_table_insert(hotblocks, (gpointer) hash, (gpointer) cnt);
     }
 
-    if (info->system_emulation && !do_inline) {
-        max_cpus = info->system.max_vcpus;
-        counts = g_ptr_array_new();
-        for (i = 0; i < max_cpus; i++) {
-            CPUCount *count = g_new0(CPUCount, 1);
-            g_mutex_init(&count->lock);
-            count->index = i;
-            g_ptr_array_add(counts, count);
-        }
-    } else if (!do_inline) {
-        g_mutex_init(&inline_count.lock);
-    }
+    g_mutex_unlock(&lock);
 
-    if (idle_report) {
-        qemu_plugin_register_vcpu_idle_cb(id, vcpu_idle);
-    }
+    /* count the number of instructions executed */
+    qemu_plugin_register_vcpu_tb_exec_inline(tb, QEMU_PLUGIN_INLINE_ADD_U64,
+                                             &cnt->exec_count, 1);
+    qemu_plugin_register_vcpu_tb_exec_inline(tb, QEMU_PLUGIN_INLINE_ADD_U64,
+                                             &inst_count, insns);
+    qemu_plugin_register_vcpu_tb_exec_cb(tb, tb_exec,
+                                         QEMU_PLUGIN_CB_NO_REGS,
+                                         (void *)hash);
+}
 
-    qemu_plugin_register_vcpu_tb_trans_cb(id, vcpu_tb_trans);
+QEMU_PLUGIN_EXPORT
+int qemu_plugin_install(qemu_plugin_id_t id, const qemu_info_t *info,
+                        int argc, char **argv)
+{
+    plugin_init();
+
+    qemu_plugin_register_vcpu_tb_trans_cb(id, tb_record);
     qemu_plugin_register_atexit_cb(id, plugin_exit, NULL);
     return 0;
 }
